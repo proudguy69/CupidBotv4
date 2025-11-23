cf3933431a3433f5a8f654018f468572f58c2319
ProudGuy69 <idioticbatteringram@gmail.com>
2025-11-23 17:31:38 -0500

    create base auth model

--- backend/models.py
+++ backend/models.py
@@ -10,3 +10,10 @@ class Profile(Model):
     age = fields.IntField()
     gender = fields.TextField()
     sexuality = fields.TextField()
+
+# represents an auth with users discord bearer token, and a JWT token thats stored in the wev 
+class Auth(Model):
+    id = fields.IntField(primary_key=True)
+    user_id = fields.IntField()
+    access_token = fields.IntField()
+    # add jwt and other here
