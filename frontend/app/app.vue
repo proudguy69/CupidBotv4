4c685e07e681425beb1ecd5afd816a647eaf99cd
Oliver <jokejupa@gmail.com>
2025-11-24 12:40:27 -0500

    create fake auth shit

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -3,5 +3,15 @@
 </template>
 
 <script setup>
+
+
+// should probably create an interface for this
+const user_object = ref({
+  user_id: null,
+  avatar: null,
+  web_token: null
+})
+
+provide("user_object", user_object)
   
 </script>
---
+++ frontend/app/pages/settings.vue
@@ -0,0 +1,40 @@
+<template>
+    <UPageSection title="settings" description="For devs only!!">
+        <UPageCard variant="subtle">
+            <UForm>
+                
+                <!-- The purpose of this form is to emulate user settings after auth-->
+                <UFormField label="User Id">
+                    <UInput v-model="user_object.user_id" />
+                </UFormField>
+
+                <UFormField label="Profile Picture">
+                    <UInput v-model="user_object.avatar" />
+                </UFormField>
+
+                <UFormField label="Web token">
+                    <UInput v-model="user_object.web_token" />
+                </UFormField>
+
+                <UFieldGroup class="mt-2">
+                    <UButton variant="soft" @click="simulateAuth">Simulate Auth</UButton>
+                    <UButton>Submit</UButton>
+                </UFieldGroup>
+
+            </UForm>
+        </UPageCard>
+    </UPageSection>
+</template>
+
+<script setup lang="ts">
+
+// this is a ref
+const user_object: any = inject("user_object")
+
+async function simulateAuth() {
+    const response = await fetch(`http://localhost:8000/authorize?code=fake`)
+    const data = await response.json()
+    console.log(data)
+}
+
+</script>
