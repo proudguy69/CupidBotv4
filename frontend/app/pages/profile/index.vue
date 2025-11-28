f4aae5594991e576c2b820a62e852f068d4d2edb
ProudGuy69 <idioticbatteringram@gmail.com>
2025-11-27 22:06:12 -0500

    create files

---
+++ frontend/app/pages/profile/index.vue
@@ -0,0 +1,9 @@
+<template>
+    
+</template>
+
+<script setup>
+    // this script file will act as a viewing tool to view ANY profile, so we should be expecting a profile id in the route query like profile/<user_id>
+    // or profile?user_id=<user_id>
+    // this will then call fetch(/profiles/get/user_id) or smth idk yet
+</script>
