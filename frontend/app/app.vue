5bc9a9bd67ba06bd917dc7440a0e9c3d8452225a
ProudGuy69 <idioticbatteringram@gmail.com>
2025-11-27 21:57:06 -0500

    finish dev auth

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -7,7 +7,16 @@
     
     <template #right>
       <UNavigationMenu :items="items" />
-      <UButton v-if="!user_object.username" variant="subtle" color="secondary" icon="logos:discord-icon">Login</UButton>
+      <UButton
+      v-if="!user_object.username"
+      variant="subtle"
+      color="secondary"
+      icon="logos:discord-icon"
+      :to="oauth2_url"
+      >
+      Login
+    </UButton>
+
       <UButton
       v-if="user_object.username"
       variant="subtle"
@@ -15,6 +24,7 @@
       :avatar="{
         src: avatar_url
       }"
+      to="/profile/settings"
       >
       {{ user_object.username }}
     </UButton>
@@ -27,10 +37,19 @@
 <script setup lang="ts">
 import type { NavigationMenuItem } from '@nuxt/ui'
 import type { User } from '~/types/User'
+import type { Authorization } from './types/Authorization'
 
 const items:NavigationMenuItem[] = [
 
 ]
+
+const oauth2_url = {
+  dev: "https://discord.com/oauth2/authorize?client_id=1442284848109846598&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauthorize&scope=identify+guilds.join+email",
+  prod: ""
+}.dev
+
+
+
 
 // should probably create an interface for this
 const user_object = ref<User>({
@@ -46,8 +65,34 @@ watch(user_object, (old:any, changed:any) => {
   avatar_url.value = `https://cdn.discordapp.com/avatars/${user_object.value.id}/${user_object.value.avatar}.png`
 })
 
+onMounted(async () => {
+  const web_token = localStorage.getItem('web_token')
+  if (!web_token) {return}
+
+  const username = localStorage.getItem('username')
+  const user_id = localStorage.getItem('user_id')
+  const avatar = localStorage.getItem('avatar')
+
+  user_object.value.id = user_id
+  user_object.value.username = username
+  user_object.value.avatar = avatar
+
+  if (username && user_id && avatar) {
+    return
+  }
+  const response = await fetch(`http://localhost:8000/authorization?token=${web_token}`)
+  const data:Authorization = await response.json()
+  if (!data.success) {
+    return
+  }
+  user_object.value = data.profile!
+})
+
 
 
 provide("user_object", user_object)
+
+
+console.log(user_object)
   
 </script>
