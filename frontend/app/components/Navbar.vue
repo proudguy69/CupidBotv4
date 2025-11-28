6332313d58ffa4a38c8cfae51416f301c36972f0
Oliver Janvier <154906790+Oliver006ug44@users.noreply.github.com>
2025-11-28 19:57:33 -0400

    UI change
    Updated the UI. (idk what I'm doing)

---
+++ frontend/app/components/Navbar.vue
@@ -0,0 +1,102 @@
+<template>
+  <UHeader>
+    <template #title>
+      Cupidv4
+    </template>
+
+    <UNavigationMenu :items="items" content-orientation="veritcal" />
+    <template #right>
+
+
+      <UButton v-if="!user_object.username" variant="subtle" color="secondary" icon="logos:discord-icon"
+        :to="oauth2_url">
+        Login
+      </UButton>
+
+      <UButton v-if="user_object.username" variant="subtle" color="secondary" :avatar="{
+        src: avatar_url
+      }" to="/profile/settings">
+        {{ user_object.username }}
+      </UButton>
+      <UColorModeButton />
+    </template>
+  </UHeader>
+  <NuxtPage />
+</template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+import type { User } from '~/types/User'
+import type { Authorization } from '../types/Authorization'
+
+const items: NavigationMenuItem[] = [
+  {
+    label: 'Profile',
+    children: [
+      {
+        label: 'Create',
+        to: '/profile/create'
+      },
+      {
+        label: 'edit',
+        to: '/profile/edit'
+      }
+    ]
+  }
+]
+
+const oauth2_url = {
+  dev: "https://discord.com/oauth2/authorize?client_id=1442284848109846598&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauthorize&scope=identify+guilds.join+email",
+  prod: ""
+}.dev
+
+
+
+
+// should probably create an interface for this
+const user_object = ref<User>({
+  id: null,
+  username: null,
+  global_name: null,
+  avatar: null,
+  web_token: null
+})
+const avatar_url = ref('')
+
+watch(user_object, (old: any, changed: any) => {
+  avatar_url.value = `https://cdn.discordapp.com/avatars/${user_object.value.id}/${user_object.value.avatar}.png`
+})
+
+onMounted(async () => {
+  const web_token = localStorage.getItem('web_token')
+  if (!web_token) { return }
+
+  const username = localStorage.getItem('username')
+  const user_id = localStorage.getItem('user_id')
+  const avatar = localStorage.getItem('avatar')
+
+  user_object.value.id = user_id
+  user_object.value.username = username
+  user_object.value.avatar = avatar
+
+  avatar_url.value = `https://cdn.discordapp.com/avatars/${user_object.value.id}/${user_object.value.avatar}.png`
+
+  if (username && user_id && avatar) {
+    return
+  }
+  const response = await fetch(`http://localhost:8000/authorization?token=${web_token}`)
+  const data: Authorization = await response.json()
+  if (!data.success) {
+    return
+  }
+  user_object.value = data.profile!
+})
+
+
+
+provide("user_object", user_object)
+
+
+console.log(user_object)
+
+</script>
